6bbe86023f4c07076bf3f1b604c132ae6ec1ab9b
hariprasad154 <hariprasas154@gmail.com>
2023-02-11 16:27:13 +0530

    Added

--- src/main/java/Com/bridgeLabs/MoodAnalyzerMain.java
+++ src/main/java/Com/bridgeLabs/MoodAnalyzerMain.java
@@ -1,4 +1,21 @@
 package Com.bridgeLabs;
 
 public class MoodAnalyzerMain {
-}
+
+
+        private String message;
+
+        // Constructor
+        public MoodAnalyzerMain(){
+        }
+        public  MoodAnalyzerMain(String message) {
+            this.message=message;
+        }
+        public String analyseMood() {
+            if (this.message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+
